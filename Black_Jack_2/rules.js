4626108da604b57c9e8b4cea0611d3e304ecbbca
AndrzejGrabarz <andgra01@gmail.com>
2022-06-05 02:20:08 +0200

    Start and Reset

--- Black_Jack_2/rules.js
+++ Black_Jack_2/rules.js
@@ -7,11 +7,32 @@ let deck;
 
 let canHit = true; // Allows the player (you) to draw while yourSum <= 21;
 let newCard;
+let startGame = document.getElementById("start-btn");
+let reset = document.getElementById("reset-btn");
+let images = document.getElementsByTagName('img');
 
-window.onload = function(){
+
+startGame.addEventListener("click",function(){
     buildDeck();
     shuffleDeck();
-}
+    startGame_player();
+    startGame_dealer();
+    
+     document.getElementById("start-btn").disabled = true;
+   
+}) 
+
+reset.addEventListener("click",function(){
+    for (let i= document.images.length; i-->0;)
+    document.images[i].parentNode.removeChild(document.images[i]);
+    document.getElementById("start-btn").disabled = false;
+}) 
+
+
+
+
+
+
 
 function buildDeck(){
     let valuesCards = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
@@ -37,3 +58,60 @@ function shuffleDeck() {
 }
 
     
+function startGame_dealer() {
+    
+       
+        cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src ="./Cards/" + card + ".png";
+        dealerSum += getValue(card);
+        cardImg.src = "./Cards/BACK.png"
+        document.getElementById("dealer-cards").append(cardImg);
+
+    for(let i = 0 ; i < 1; i++) {
+        
+        cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src ="./Cards/" + card + ".png";
+        dealerSum += getValue(card);
+        document.getElementById("dealer-cards").append(cardImg);
+        
+        
+        
+    }
+    
+console.log("Krupier " + dealerSum)
+      
+}
+
+function startGame_player() {
+    
+    for(let i = 0 ; i < 2; i++) {
+        
+        cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src ="./Cards/" + card + ".png";
+        yourSum += getValue(card);
+        document.getElementById("your-cards").append(cardImg);
+        
+    }
+      console.log("Ty " + yourSum)
+}
+
+
+
+
+
+function getValue(card) {
+    let data = card.split("-");
+    let value = data[0];
+
+    if(isNaN(value)){
+        if(value == "A"){
+            return 11
+        }
+        return 10;
+    }
+    return parseInt(value)
+   
+ }
